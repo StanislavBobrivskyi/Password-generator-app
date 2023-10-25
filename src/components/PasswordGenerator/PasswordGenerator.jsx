af0389b50d20b3256ba2cabb2c974508c0626894
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-10-25 09:08:32 +0200

    working on main component

--- src/components/PasswordGenerator/PasswordGenerator.jsx
+++ src/components/PasswordGenerator/PasswordGenerator.jsx
@@ -3,18 +3,30 @@ import { PasswordLength } from '../PasswordLength/PasswordLength';
 import { Checkbox } from '../Checkbox/Checkbox';
 import { PasswordStrength } from '../PasswordStrength/PasswordStrength';
 import { ButtonGenerator } from '../ButtonGenerator/ButtonGenerator';
-
+import { Formik, Field, Form } from 'formik';
 export function PasswordGenerator() {
   return (
     <div>
-      <h1>Password Generator</h1>
-      <PasswordLength />
-      <Checkbox label="Uppercase Letters" />
-      <Checkbox label="Lowercase Letters" />
-      <Checkbox label="Numbers" />
-      <Checkbox label="Special Characters" />
-      <PasswordStrength />
-      <ButtonGenerator />
+      <Formik
+        initialValues={{ password: '' }}
+        onSubmit={(values, { resetForm }) => {
+          // Додайте логіку генерації паролю тут
+          resetForm();
+        }}
+      >
+        <Form>
+          <div>
+            <Field type="text" name="password" readOnly />
+          </div>
+          <PasswordLength />
+          <Checkbox label="Uppercase Letters" />
+          <Checkbox label="Lowercase Letters" />
+          <Checkbox label="Numbers" />
+          <Checkbox label="Special Characters" />
+          <PasswordStrength />
+          <ButtonGenerator />
+        </Form>
+      </Formik>
     </div>
   );
 }
