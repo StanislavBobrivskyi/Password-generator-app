feafa57682630d62d80d81b58e31623a19d73bb9
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-10-25 21:19:23 +0200

    colored Strength password style

--- src/components/PasswordStrength/PasswordStrength.jsx
+++ src/components/PasswordStrength/PasswordStrength.jsx
@@ -1,23 +1,119 @@
+// import React from 'react';
+// import styled from 'styled-components';
+
+// const StrengthSquare = styled.div`
+//   width: 20px;
+//   height: 20px;
+//   margin: 0 5px;
+//   border: 1px solid #000;
+//   background-color: ${props =>
+//     props.isFilled ? props.fillColor : 'transparent'};
+// `;
+
+// const StrengthSquaresContainer = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
+
+// export function PasswordStrength({ password }) {
+//   function getPasswordStrength(password) {
+//     if (password.length < 8) {
+//       return '';
+//     } else if (password.length < 10) {
+//       return 'TO WEAK!';
+//     } else if (password.length < 12) {
+//       return 'WEAK';
+//     } else if (password.match(/[0-9]/) && password.match(/[a-zA-Z]/)) {
+//       return 'MEDIUM';
+//     } else {
+//       return 'STRONG';
+//     }
+//   }
+
+//   const strength = getPasswordStrength(password);
+
+//   return (
+//     <div>
+//       <p>Password Strength: {strength}</p>
+//       <StrengthSquaresContainer>
+//         <StrengthSquare isFilled={strength === 'TO WEAK!'} fillColor="red" />
+//         <StrengthSquare isFilled={strength === 'WEAK'} fillColor="orange" />
+//         <StrengthSquare isFilled={strength === 'MEDIUM'} fillColor="yellow" />
+//         <StrengthSquare isFilled={strength === 'STRONG'} fillColor="green" />
+//       </StrengthSquaresContainer>
+//     </div>
+//   );
+// }
+
 import React from 'react';
+import styled from 'styled-components';
+
+const StrengthSquare = styled.div`
+  width: 20px;
+  height: 20px;
+  margin: 0 5px;
+  border: 1px solid #000;
+  background-color: ${props =>
+    props.isFilled ? props.fillColor : 'transparent'};
+`;
+
+const StrengthSquaresContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
 export function PasswordStrength({ password }) {
   function getPasswordStrength(password) {
     if (password.length < 8) {
-      return 'Password Strength: Very Weak';
+      return '';
+    } else if (password.length < 10) {
+      return 'TO WEAK!';
     } else if (password.length < 12) {
-      return 'Password Strength: Weak';
+      return 'WEAK';
     } else if (password.match(/[0-9]/) && password.match(/[a-zA-Z]/)) {
-      return 'Password Strength: Medium';
+      return 'MEDIUM';
     } else {
-      return 'Password Strength: Strong';
+      return 'STRONG';
     }
   }
 
   const strength = getPasswordStrength(password);
 
+  // Визначення рівня міцності
+  let level;
+  if (strength === '') {
+    return '';
+  } else if (strength === 'TO WEAK!') {
+    level = 1;
+  } else if (strength === 'WEAK') {
+    level = 2;
+  } else if (strength === 'MEDIUM') {
+    level = 3;
+  } else {
+    level = 4;
+  }
+
   return (
     <div>
-      <p>{strength}</p>
+      <p>Password Strength: {strength}</p>
+      <StrengthSquaresContainer>
+        <StrengthSquare
+          isFilled={level >= 1}
+          fillColor={level >= 1 ? 'red' : 'transparent'}
+        />
+        <StrengthSquare
+          isFilled={level >= 2}
+          fillColor={level >= 2 ? 'orange' : 'transparent'}
+        />
+        <StrengthSquare
+          isFilled={level >= 3}
+          fillColor={level >= 3 ? 'yellow' : 'transparent'}
+        />
+        <StrengthSquare
+          isFilled={level >= 4}
+          fillColor={level >= 4 ? 'green' : 'transparent'}
+        />
+      </StrengthSquaresContainer>
     </div>
   );
 }
