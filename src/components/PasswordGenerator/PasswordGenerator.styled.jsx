18f9328a631de0de170e07a2821bb16a9da2bb42
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-10-28 13:56:56 +0200

    styled

--- src/components/PasswordGenerator/PasswordGenerator.styled.jsx
+++ src/components/PasswordGenerator/PasswordGenerator.styled.jsx
@@ -1,9 +1,10 @@
+import { Field } from 'formik';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
   width: 540px;
   height: 528px;
-  background: #24232c;
+  /* background: #24232c; */
   display: flex;
   justify-content: center;
 `;
@@ -11,4 +12,50 @@ export const CheckBoxWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  height: 80px;
+
+  background-color: #24232c;
+  position: relative;
+  margin-bottom: 24px;
+`;
+
+export const StyledInput = styled(Field)`
+  padding: 19px 32px;
+  color: #e6e5ea;
+  font-feature-settings: 'clig' off, 'liga' off;
+
+  /* Heading (L) */
+  font-family: JetBrains Mono;
+  font-size: 32px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background-color: transparent;
+  border: none;
+`;
+
+export const CopyBtn = styled.button`
+  border: none;
+  position: absolute;
+  transform: translate(0, 0);
+  top: 40%;
+  right: 10%;
+  padding: 0;
+  width: 21px;
+  height: 24px;
+  background: transparent;
+  &:hover {
+    color: #24232c;
+  }
+`;
+
+export const MainBlock = styled.div`
+  background-color: #24232c;
+  padding: 19px 32px;
 `;
